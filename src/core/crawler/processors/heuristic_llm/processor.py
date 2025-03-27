52620b93dbfa319325071760272a9f07916f26f5
EnejM <68961385+menej@users.noreply.github.com>
2025-03-27 22:17:19 +0100

    feat: Update chrome version in user agent
    The new version 134 also matches the current Playwright Chrome driver version.

--- src/core/crawler/processors/heuristic_llm/processor.py
+++ src/core/crawler/processors/heuristic_llm/processor.py
@@ -210,7 +210,7 @@ def _init_driver():
         user_agent=(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/110.0.0.0 Safari/537.36"
+            "Chrome/134.0.0.0 Safari/537.36"
         )
     )
 
